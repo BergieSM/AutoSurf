e5b94c6f69375df730a36ce893f5009bd163e0be
BergieSM <BergieSM@Gmail.com>
2015-08-28 13:24:23 -0400

    Minor update to switch the ExperimentHere class to use Chrome, Firefox has updated and broken workflow for all Selenium users currently.
    Bergie, 8/28/15 1:30 PM

--- src/AutoSurf/ExperimentHere.java
+++ src/AutoSurf/ExperimentHere.java
@@ -16,15 +16,13 @@ public class ExperimentHere extends TestCases {
 	 * This function goes to the 2qa Main Admin page on Dev, and prints the title to the console and Results file.
 	 */
 	protected static void goTo2QAMainAdmin() {
-		/*
-		 * WebDriver driver = Driver.getCurrentDriver();
+		WebDriver driver = Driver.getCurrentDriver();
 		Parameters.setBoxSet(2);	//Sets the main Parameter's boxSet to 2, Dev. 
 		Navigation.goToSuperGlobal(); // Goes to the superglobaladmin site based on the Parameter's BoxSet setting, set just above.
 		Parameters.setMSP("2_qa"); // Set the MSP to navigate to.
 		SuperGlobalAdmin.MSPAdminLinkSelect(); // Select the Admin Link on the SuperGlobalAdmin page for the selected MSP.
 		System.out.println("Expecting to see something about 2qa: "	+ driver.getTitle()); // This will output to the console the first line in double quotes, appended with the title of the webpage, provided by the driver.
 		FileOutput.throwInResults("Expecting to see something about 2qa: " + driver.getTitle()); // Sending the same to the Results file.
-		*/
 	}
 	
 	/**
@@ -32,7 +30,7 @@ public class ExperimentHere extends TestCases {
 	 */
 	public static void main() {
 		// Write whatever you like into this space to have it run.
-		Driver.switchToFirefox(); //This launches a Firefox window under WebDriver control.
+		Driver.switchToChrome(); //This launches a Chrome window under WebDriver control.
 		WebDriver driver = Driver.getCurrentDriver();	//This loads the current WenDriver (firefox window) as 'driver' for easier control.
 		driver.get("https://www.google.com/"); //Load the google home page.
 		FileOutput.throwInResults(driver.getTitle()); // Have the driver get the title of the google homepage, and send that to the Results file.
